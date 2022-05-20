9d38f935074071cbe5ea5089cd530d54749949b1
Valeria Stamenova <valeria.stamenova3@gmail.com>
2022-05-20 19:20:53 +0300

    Create application index

---
+++ Web/Controllers/ApplicationController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+	public class ApplicationController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult AfterSeventhGrade()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult AfterFourthGrade()
+		{
+			return View();
+		}
+	}
+}
